9a1b0e59abef9778698b934c63c74065632f2ea1
Even Ambjørnrud <even.ambjornrud@entur.org>
2022-04-12 12:44:36 +0200

    <feat>(Telldus Local): Add the ability to change the auth-token
    jira:
    - First version that adds two new device settings, Host and Auth_Token.

--- drivers/ClientLocal/device.js
+++ drivers/ClientLocal/device.js
@@ -8,12 +8,18 @@ const constants = require('../../lib/telldus-api/constants');
 module.exports = class ClientLocalDevice extends BaseClientDevice {
 
     onInit() {
-        const keys = this.getStoreValue('keys');
-        const host = keys.ip_address;
-        const accessToken = keys.access_token;
-        const log = this.log;
-        this._api = new LocalApi({ host, accessToken, log });
-        this.scheduleCheckData(5);
+      const keys = this.getStoreValue('keys');
+      const host = keys.ip_address;
+      const accessToken = keys.access_token;
+      const log = this.log;
+      this._api = new LocalApi({ host, accessToken, log });
+
+      const settings = {
+        Auth_Token: accessToken,
+        Host: host
+      };
+      const promise = this.setSettings(settings);
+      this.scheduleCheckData(5);
     }
 
     getShortName() {
@@ -47,12 +53,13 @@ module.exports = class ClientLocalDevice extends BaseClientDevice {
 
     async fetchSensorValues() {
         const sensors = await this._api.listSensors({});
+        this.log("sensors: " + JSON.stringify(sensors, null, 2));
         Homey.app.sensorValues(sensors);
     }
 
     async fetchDevicesValues() {
-        const devices = await this._api.listDevices({
-        })
+        const devices = await this._api.listDevices({});
+//    this.log("devices: " + JSON.stringify(devices, null, 2));
         Homey.app.deviceValues(devices);
     }
 
